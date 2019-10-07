10fc7eff42f683ef1eb2042f94e8a4e47748fb55
botagari <48246327+botagari@users.noreply.github.com>
2019-10-07 21:27:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,36 +27,12 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = '#';
+const prefix = 'fadfg';
 var table = require('table').table
 var ti={}  
 ,spee={}
 ,attentions={};
 
-client.on('ready', function(){
-    var ms = 60000 ;
-    var setGame = ['#help','Type #help'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/barontube`);
-    }, ms);
-    console.log(` ????????? |> Name: ${client.user.username}`);
- console.log(` ????????? |> Servers: ${client.guilds.size}`);
- console.log(` ???????????????????? |> Members: ${client.users.size}`);
- console.log(` ????????????????????? |> Channels: ${client.channels.size}`);
- console.log(` ???????????????????? |> Channels: ${client.channels.size}`);
- console.log(` ???????????????????? |> Id: ${client.user.id}`);
- console.log(` ???????????????????`);
- console.log(` ???????????????????`);
-});
 client.on('message', message => {
 if (message.content.startsWith(prefix + 'help')) { /// This is The DMS Code Send The Help In DMS // Code By NotGucci
     let pages = [`**
@@ -199,28 +175,6 @@ ${prefix}voiceonline / لتفعيل روم الفويس اونلاين
     })
     }
 }); 
-
-
-/*الشعار*/
-
-const secreT = [
-  "**[ ! KD, ] : شعارك بالديسكورد [ !ᵏᵈを ] :شعارك باللعبه **"
-]
-
-
- client.on('message', message => {
-   if (message.content.startsWith("الشعار")) {
-                if(!message.channel.guild) return message.reply('** This command only for servers**');
-  var embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-
-   .setThumbnail(message.author.avatarURL) 
- .addField('لعبه خواطر' ,
-  `${secreT[Math.floor(Math.random() * secreT.length)]}`)
-  message.channel.sendEmbed(embed);
-  console.log('[id] Send By: ' + message.author.username)
-    }
-});
 
 const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
 const coolDown = new Set();
@@ -743,9 +697,10 @@ message.channel.send(
   message.channel.sendEmbed(embed);
     }
 });
+
 client.on('message' , message => {
 if (message.author.bot) return;
-if (message.content.startsWith(prefix + "contact")) {
+if (message.content.startsWith("!status")) {
 if (!message.channel.guild) return;
 
 
@@ -753,7 +708,7 @@ if (!message.channel.guild) return;
 let args = message.content.split(" ").slice(1).join(" ");
 
 
-client.users.get("516473846983950336").send(
+client.users.get("567694723679125514").send(
     "\n" + "**" + "? السيرفر :" + "**" +
     "\n" + "**" + "» " + message.guild.name + "**" +
     "\n" + "**" + " ? المرسل : " + "**" +
@@ -763,9 +718,9 @@ client.users.get("516473846983950336").send(
 
 let embed = new Discord.RichEmbed()
      .setAuthor(message.author.username, message.author.avatarURL)
-     .setDescription(':mailbox_with_mail: تم ارسال الرسالة الى صاحب البوت بنجاح')
+     .setDescription('**start status in your account in 5 minutes**')
      .setThumbnail(message.author.avatarURL)
-     .setFooter("By : 3bker")
+     .setFooter("Dicord Status")
                                                 
 
 message.channel.send(embed);
